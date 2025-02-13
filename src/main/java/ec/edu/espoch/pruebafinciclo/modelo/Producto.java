f18313527b7340277d429f5b867f51c4c4365f59
WilliAn-333 <www.pegazo23@gmail.com>
2025-02-13 14:34:01 -0500

    clases

--- src/main/java/ec/edu/espoch/pruebafinciclo/modelo/Producto.java
+++ src/main/java/ec/edu/espoch/pruebafinciclo/modelo/Producto.java
@@ -9,5 +9,34 @@ package ec.edu.espoch.pruebafinciclo.modelo;
  * @author USER
  */
 public class Producto {
+    private int id;
+    private String Nombre;
+    private double Precio;
+    private boolean Disponible;
+
+    public Producto(int id, String Nombre, double Precio, boolean Disponible) {
+        this.id = id;
+        this.Nombre = Nombre;
+        this.Precio = Precio;
+        this.Disponible = Disponible;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public double getPrecio() {
+        return Precio;
+    }
+
+    public boolean isDisponible() {
+        return Disponible;
+    }
+    
+    
     
 }
